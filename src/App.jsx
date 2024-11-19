08314821c5df618abf6e5cb98fbc771148aac5cc
Guru <gurupreethnagesha@gmail.com>
2024-11-19 17:07:23 +0530

    Default boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import RegisterOrgan from "./Pages/RegisterOrgan";
+import LoginOrgan from "./Pages/LoginOrgan";
+import Role from "./Pages/Role";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Role />} />
+            <Route path="/user" element={<Login />} />
+            <Route path="/organ" element={<LoginOrgan />} />
+            <Route path="/register/user" element={<Register />} />
+            <Route path="/register/organ" element={<RegisterOrgan />} />
+          </Routes>
+        </Router>
+      </Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
