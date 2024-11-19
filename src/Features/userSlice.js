45934c5737c97619f21dbfc0b4996a39e0605483
Guru <gurupreethnagesha@gmail.com>
2024-11-19 19:33:09 +0530

    changed store by adding theme

--- src/Features/userSlice.js
+++ src/Features/userSlice.js
@@ -1,16 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
-
 const userSlice = createSlice({
   name: "user",
   initialState: {
     role: null,
+    theme: "dim",
   },
   reducers: {
     setUserRole: (state, action) => {
       state.role = action.payload;
+      if (action.payload === "user") {
+        state.theme = "autumn";
+      } else if (action.payload === "organ") {
+        state.theme = "lemonade";
+      }
+    },
+    setUserTheme: (state, action) => {
+      state.theme = action.payload;
     },
   },
 });
 
-export const { setUserRole } = userSlice.actions;
+export const { setUserRole, setUserTheme } = userSlice.actions;
 export default userSlice.reducer;
